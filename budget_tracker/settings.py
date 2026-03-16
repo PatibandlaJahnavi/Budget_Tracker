e32283018395c26ed8b182f564c6423e38312911
PatibandlaJahnavi <jahnavi.patibandla2003@gmail.com>
2026-03-16 14:15:21 +0000

    code

--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -20,12 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eh!mbejwhlw8%!c8%t(i6zs$cbz_p+(r*noas1^mew8obiw1gg'
+key = None
+with open('secret.key') as f:
+    key = f.read().strip()
+
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fintrack.pythonanywhere.com']
 
 
 # Application definition
